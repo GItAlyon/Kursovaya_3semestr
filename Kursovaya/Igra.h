b6523a0d78e8a1db5c171324311ef0bf98ae0a34
Alyona Shevchenko <alyona070106@gmail.com>
2025-01-22 03:16:37 +0700

    Добавление картинок

--- Kursovaya/Igra.h
+++ Kursovaya/Igra.h
@@ -38,6 +38,12 @@ namespace Kursovaya {
 			}
 		}
 	private: System::Windows::Forms::Button^ Menu;
+	private: System::Windows::Forms::ImageList^ imageList1;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
+
+
+	private: System::ComponentModel::IContainer^ components;
 	protected:
 
 	protected:
@@ -46,7 +52,7 @@ namespace Kursovaya {
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -55,29 +61,61 @@ namespace Kursovaya {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Igra::typeid));
 			this->Menu = (gcnew System::Windows::Forms::Button());
+			this->imageList1 = (gcnew System::Windows::Forms::ImageList(this->components));
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// Menu
 			// 
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->Menu->Location = System::Drawing::Point(12, 12);
+			this->Menu->BackColor = System::Drawing::Color::Transparent;
+			this->Menu->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->Menu->Font = (gcnew System::Drawing::Font(L"Mistral", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->Menu->ForeColor = System::Drawing::Color::Maroon;
+			this->Menu->Location = System::Drawing::Point(55, 99);
+			this->Menu->Margin = System::Windows::Forms::Padding(2);
 			this->Menu->Name = L"Menu";
-			this->Menu->Size = System::Drawing::Size(181, 42);
+			this->Menu->Size = System::Drawing::Size(220, 50);
 			this->Menu->TabIndex = 0;
 			this->Menu->Text = L"Главное меню";
-			this->Menu->UseVisualStyleBackColor = true;
+			this->Menu->UseVisualStyleBackColor = false;
 			this->Menu->Click += gcnew System::EventHandler(this, &Igra::Menu_Click);
+			// 
+			// imageList1
+			// 
+			this->imageList1->ImageStream = (cli::safe_cast<System::Windows::Forms::ImageListStreamer^>(resources->GetObject(L"imageList1.ImageStream")));
+			this->imageList1->TransparentColor = System::Drawing::Color::Transparent;
+			this->imageList1->Images->SetKeyName(0, L"перс.jpg");
+			this->imageList1->Images->SetKeyName(1, L"перс1.jpg");
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.BackgroundImage")));
+			this->pictureBox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->pictureBox1->Location = System::Drawing::Point(529, 175);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(738, 416);
+			this->pictureBox1->TabIndex = 1;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->WaitOnLoad = true;
 			// 
 			// Igra
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(642, 356);
+			this->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->ClientSize = System::Drawing::Size(1924, 1061);
+			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->Menu);
+			this->Margin = System::Windows::Forms::Padding(2);
 			this->Name = L"Igra";
 			this->Text = L"Igra";
 			this->Load += gcnew System::EventHandler(this, &Igra::Igra_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 
 		}
--- Kursovaya/MyForm.h
+++ Kursovaya/MyForm.h
@@ -1,7 +1,14 @@
 #pragma once
 #include <Windows.h>
 #include <stdio.h>
-#include <locale.h>
+#define _CRT_SECURE_NO_WARNINGS   
+#include <stdio.h> 
+#include <locale.h> 
+#include <conio.h> 
+#include <math.h> 
+#include <malloc.h>
+#include <ctype.h>
+#include <string.h>
 #include "Igra.h"
 
 namespace Kursovaya {
@@ -37,10 +44,13 @@ namespace Kursovaya {
 			{
 				delete components;
 			}
+
 		}
 	private: System::Windows::Forms::Button^ Nach_igra;
 	protected:
 	private: System::Windows::Forms::Button^ Exit;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
 
 	private:
 		/// <summary>
@@ -55,45 +65,79 @@ namespace Kursovaya {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->Nach_igra = (gcnew System::Windows::Forms::Button());
 			this->Exit = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// Nach_igra
 			// 
-			this->Nach_igra->Location = System::Drawing::Point(332, 117);
+			this->Nach_igra->BackColor = System::Drawing::Color::Transparent;
+			this->Nach_igra->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->Nach_igra->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->Nach_igra->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->Nach_igra->Font = (gcnew System::Drawing::Font(L"Mistral", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->Nach_igra->ForeColor = System::Drawing::Color::Maroon;
+			this->Nach_igra->Location = System::Drawing::Point(218, 190);
+			this->Nach_igra->Margin = System::Windows::Forms::Padding(2);
 			this->Nach_igra->Name = L"Nach_igra";
-			this->Nach_igra->Size = System::Drawing::Size(158, 46);
+			this->Nach_igra->Size = System::Drawing::Size(286, 64);
 			this->Nach_igra->TabIndex = 0;
 			this->Nach_igra->Text = L"Начать игру";
-			this->Nach_igra->UseVisualStyleBackColor = true;
+			this->Nach_igra->UseVisualStyleBackColor = false;
 			this->Nach_igra->Click += gcnew System::EventHandler(this, &MyForm::Nach_igra_Click);
 			// 
 			// Exit
 			// 
-			this->Exit->Location = System::Drawing::Point(354, 251);
+			this->Exit->BackColor = System::Drawing::Color::Transparent;
+			this->Exit->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->Exit->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->Exit->Font = (gcnew System::Drawing::Font(L"Mistral", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->Exit->ForeColor = System::Drawing::Color::Maroon;
+			this->Exit->Location = System::Drawing::Point(285, 284);
+			this->Exit->Margin = System::Windows::Forms::Padding(2);
 			this->Exit->Name = L"Exit";
-			this->Exit->Size = System::Drawing::Size(117, 61);
+			this->Exit->Size = System::Drawing::Size(143, 64);
 			this->Exit->TabIndex = 1;
 			this->Exit->Text = L"Выход";
-			this->Exit->UseVisualStyleBackColor = true;
+			this->Exit->UseVisualStyleBackColor = false;
 			this->Exit->Click += gcnew System::EventHandler(this, &MyForm::Exit_Click);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.BackgroundImage")));
+			this->pictureBox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->pictureBox1->Location = System::Drawing::Point(175, 125);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(1434, 900);
+			this->pictureBox1->TabIndex = 3;
+			this->pictureBox1->TabStop = false;
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(900, 375);
+			this->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->ClientSize = System::Drawing::Size(1831, 1009);
 			this->Controls->Add(this->Exit);
 			this->Controls->Add(this->Nach_igra);
+			this->Controls->Add(this->pictureBox1);
+			this->KeyPreview = true;
+			this->Margin = System::Windows::Forms::Padding(2);
 			this->Name = L"MyForm";
 			this->Text = L"Главное меню";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+		
 	}
 	private: System::Void Exit_Click(System::Object^ sender, System::EventArgs^ e) {
 		exit(0);
@@ -104,4 +148,6 @@ namespace Kursovaya {
 		//this->Visible = FALSE;
 	}
 	};
+
+
 }
